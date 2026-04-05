4b129d2fc88a2484a64014116b7e73ffd10eaeb8
Manoj H P <manumanoj69297@gmail.com>
2026-04-05 20:19:39 +0530

    adding sitemap url

--- server.js
+++ server.js
@@ -38,6 +38,29 @@ app.get('/api/config', (req, res) => {
   });
 });
 
+app.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://ledgestay.in/</loc>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://ledgestay.in/about</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://ledgestay.in/listings</loc>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://ledgestay.in/contact</loc>
+    <priority>0.7</priority>
+  </url>
+</urlset>`);
+});
+
 // Catch-all for ALL non-API routes (SPA entrypoint)
 app.get(/^\/(?!api).*/, (req, res) => {
   return res.sendFile(path.join(__dirname, 'public', 'index.html'));
